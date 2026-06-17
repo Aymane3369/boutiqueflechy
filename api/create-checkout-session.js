77feb0aa304c442f93521caedff06b8339062180
Aymane3369 <168319843+Aymane3369@users.noreply.github.com>
2026-06-17 11:59:49 +0100

    Update create-checkout-session.js

--- api/create-checkout-session.js
+++ api/create-checkout-session.js
@@ -34,7 +34,7 @@ module.exports = async (req, res) => {
 
       // === COLLECTE AUTOMATIQUE DES INFORMATIONS CLIENT ===
       // 1. Email du client (pré-rempli ou demandé par Stripe)
-       customer_email: ''
+       customer_email: '',
      
 
       // 2. Adresse de livraison (Stripe affiche les champs)
